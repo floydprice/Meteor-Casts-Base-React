d38dfb1860507e34e4ecfc54d6c58d4e01aa3ade
Floyd Price <f.price@floydprice.com>
2016-03-03 10:20:55 +0000

    initial commit

---
+++ app/home/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Home extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <a href="/foo">Blaze?</a>
+      </div>
+    )
+  }
+}
+
+export default Home
